89739a2b8ffc70d1666e1f4489a7b530bb2fa26b
安安 <daidaiyidaidaidai@163.com>
2022-02-26 15:50:23 +0800

    模块化

---
+++ src/components/money/Notes.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label class="notes">
+      <span class="name">备注</span>
+      <input type="text" placeholder="在这里输入备注" />
+    </label>
+  </div>
+</template>
+
+<style lang="scss">
+.notes {
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  background: #f5f5f5;
+  padding-left: 16px;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/components/money/Tags.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="tags">
+    <div class="new">
+      <button>新增标签</button>
+    </div>
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+@import "~@/assets/style/help.scss";
+
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+  flex-grow: 1;
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      $h: 24px;
+      background: #d9d9d9;
+      height: $h;
+      border-radius: calc(24px / 2);
+      padding: 0 16px;
+      margin-right: 12px;
+      line-height: $h;
+      margin-top: 4px;
+    }
+  }
+  > .new {
+    padding-top: 16px;
+    button {
+      background: transparent;
+      border: none;
+      border-bottom: 1px solid;
+      padding: 0 5px;
+    }
+  }
+}
+</style>
