1060ff5ce7447fa2c3c5241ead6284d7a41f5225
安安 <daidaiyidaidaidai@163.com>
2022-02-26 15:23:49 +0800

    money 页面的 HTML 和 css 写完

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,15 +1,196 @@
 <template>
-  <layout>
-    <p>money</p>
+  <layout class-prefix="xxx">
+    <div class="numberPad">
+      <div class="output">100</div>
+      <div class="buttons">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>删除</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>清空</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button class="ok">OK</button>
+        <button class="zero">0</button>
+        <button class="spot">.</button>
+      </div>
+    </div>
+    <div>
+      <ul class="types">
+        <li class="selected">支出</li>
+        <li>收入</li>
+      </ul>
+    </div>
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="在这里输入备注" />
+      </label>
+    </div>
+    <div class="tags">
+      <div class="new">
+        <button>新增标签</button>
+      </div>
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
+    </div>
   </layout>
 </template>
 
 <script lang="ts">
 import Layout from "@/components/Layout.vue";
-import Vue from "vue";
 
-export default Vue.extend({
+export default {
   components: { Layout },
-});
+};
 </script>
 
+<style lang="scss">
+.xxx-wrapper {
+  color: inherit;
+}
+.xxx-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
+</style>
+
+<style lang="scss" scoped>
+@import "~@/assets/style/help.scss";
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+  flex-grow: 1;
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      $h: 24px;
+      background: #d9d9d9;
+      height: $h;
+      border-radius: calc(24px / 2);
+      padding: 0 16px;
+      margin-right: 12px;
+      line-height: $h;
+      margin-top: 4px;
+    }
+  }
+  > .new {
+    padding-top: 16px;
+    button {
+      background: transparent;
+      border: none;
+      border-bottom: 1px solid;
+      padding: 0 5px;
+    }
+  }
+}
+.notes {
+  display: flex;
+  align-items: center;
+  font-size: 14px;
+  background: #f5f5f5;
+  padding-left: 16px;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    display: flex;
+    height: 64px;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected {
+      &::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333;
+      }
+    }
+  }
+}
+.numberPad {
+  .output {
+    @extend %innerShadow;
+    font-size: 36px;
+    font-family: Consolas, monospace;
+    padding: 9px 16px;
+    text-align: right;
+  }
+  .buttons {
+    @extend %clearFix;
+    button {
+      font-size: 18px;
+      width: 25%;
+      height: 64px;
+      float: left;
+      &.ok {
+        height: 64 * 2px;
+        float: right;
+      }
+      &.zero {
+        width: 50%;
+      }
+      &.spot {
+        font-weight: 900;
+      }
+      &:nth-child(1) {
+        background: $bg;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        background: darken($bg, 4%);
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background: darken($bg, 4 * 3%);
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background: darken($bg, 4 * 4%);
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background: darken($bg, 4 * 5%);
+      }
+      &:nth-child(14) {
+        background: darken($bg, 4 * 6%);
+      }
+      &:nth-child(12) {
+        background: darken($bg, 4 * 7%);
+      }
+    }
+  }
+}
+</style>
