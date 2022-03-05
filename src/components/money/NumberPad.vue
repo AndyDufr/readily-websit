e09c5ac42243e72f8f254c6ee7db3d0fb13c7985
安安 <daidaiyidaidaidai@163.com>
2022-03-05 10:58:17 +0800

    基本完成 NumberPad 组件

--- src/components/money/NumberPad.vue
+++ src/components/money/NumberPad.vue
@@ -1,24 +1,56 @@
 <template>
   <div class="numberPad">
-    <div class="output">100</div>
+    <div class="output">{{ output || "0" }}</div>
     <div class="buttons">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>清空</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button class="ok">OK</button>
-      <button class="zero">0</button>
-      <button class="spot">.</button>
+      <button @click="inputContent">1</button>
+      <button @click="inputContent">2</button>
+      <button @click="inputContent">3</button>
+      <button @click="backOff">删除</button>
+      <button @click="inputContent">4</button>
+      <button @click="inputContent">5</button>
+      <button @click="inputContent">6</button>
+      <button @click="empty">清空</button>
+      <button @click="inputContent">7</button>
+      <button @click="inputContent">8</button>
+      <button @click="inputContent">9</button>
+      <button @click="ok" class="ok">OK</button>
+      <button @click="inputContent" class="zero">0</button>
+      <button @click="inputContent" class="spot">.</button>
     </div>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+@Component
+export default class NumberPad extends Vue {
+  output = "0";
+  inputContent(event: MouseEvent): void {
+    const text = (event.target as HTMLButtonElement).textContent as string;
+    if (this.output.length === 16) return;
+    if (this.output === "0") {
+      if ("0123456789".indexOf(text) >= 0) {
+        this.output = text;
+      } else {
+        this.output += text;
+      }
+      return;
+    }
+    if (this.output.indexOf(".") >= 0 && text === ".") return;
+    this.output += text;
+  }
+  backOff(): void {
+    this.output = this.output.slice(0, -1);
+  }
+  empty(): void {
+    this.output = "0";
+  }
+  ok(): void {
+    console.log("todo");
+  }
+}
+</script>
 
 <style lang="scss">
 @import "~@/assets/style/help.scss";
--- src/components/money/Types.vue
+++ src/components/money/Types.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' ? 'selected' : ''" @click="selectType('-')">
+        支出
+      </li>
+      <li :class="type === '+' ? 'selected' : ''" @click="selectType('+')">
+        收入
+      </li>
     </ul>
   </div>
 </template>
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
 
+@Component
+export default class Types extends Vue {
+  type = "-";
+  selectType(type: "-" | "+"): void {
+    if (type !== "-" && type !== "+") {
+      throw new Error("type is unkown");
+    }
+    this.type = type;
+  }
+}
+</script>
 <style lang="scss">
 .types {
   background: #c4c4c4;
