af1797436dd57b6065dab105d3160706f20396de
安安 <daidaiyidaidaidai@163.com>
2022-03-10 16:46:29 +0800

    新增 edit 页面， 通过 tag 的 id 来判断需要修改的标签

--- src/components/labels/Edit.vue
+++ src/components/labels/Edit.vue
@@ -3,8 +3,21 @@
 </template>
 
 <script lang="ts">
+import tagListModel from "@/models/tagListModel";
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 @Component
-export default class Edit extends Vue {}
+export default class Edit extends Vue {
+  created(): void {
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    const tag = tags.filter((item) => item.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace("/404");
+    }
+  }
+}
 </script>
