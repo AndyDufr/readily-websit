4c1e763dc0ad3d2f4ce8abba72f6bb334843e505
安安 <daidaiyidaidaidai@163.com>
2022-03-10 16:59:43 +0800

    完成 点击标签跳转至编辑页面

--- src/components/labels/Edit.vue
+++ src/components/labels/Edit.vue
@@ -9,9 +9,12 @@ import { Component } from "vue-property-decorator";
 @Component
 export default class Edit extends Vue {
   created(): void {
+    // 通过 this.$route.id 来获取地址栏中的 id。id 是在路由中的 :id 占位符来定义的
     const id = this.$route.params.id;
+
     tagListModel.fetch();
     const tags = tagListModel.data;
+    // 通过 filter 找出数据库中 标签的 id 和地址栏的 id 一样的标签
     const tag = tags.filter((item) => item.id === id)[0];
     if (tag) {
       console.log(tag);
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -5,7 +5,7 @@
         class="tag"
         v-for="item in tags"
         :key="item.id"
-        :to="`/labels/${item.id}`"
+        :to="`/labels/edit/${item.id}`"
       >
         <span>{{ item.name }}</span>
         <Icon name="right" />
@@ -21,11 +21,9 @@
 import Icon from "@/components/Icon.vue";
 import Layout from "@/components/Layout.vue";
 import tagListModel from "@/models/tagListModel";
-import { nanoid } from "nanoid";
 
 // 每次先提前获取一下| 这句必须在tags定义之前执行
 tagListModel.fetch();
-console.log(nanoid());
 
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
