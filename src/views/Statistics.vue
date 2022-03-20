d7d091e8cbff247a2e53ace0f7cf2cb560b45a9b
Andy <daidaiyidaidaidai@163.com>
2022-03-20 15:53:33 +0800

    使用 deep 语法 实现 statistics 页面的 types 组件的切换功能

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,13 +1,36 @@
 <template>
   <layout>
-    <p>statistics</p>
+    <Types class="x" :value.sync="value" />
   </layout>
 </template>
 
 <script lang="ts">
 import Layout from "@/components/Layout.vue";
+import Types from "@/components/money/Types.vue";
 import Vue from "vue";
-export default Vue.extend({
-  components: { Layout },
-});
+import { Component } from "vue-property-decorator";
+@Component({
+  components: { Layout, Types },
+})
+export default class Statistics extends Vue {
+  value = "-";
+}
 </script>
+
+<style lang="scss" scoped>
+.x ::v-deep li {
+  background: #fff;
+  &.selected {
+    background: #c4c4c4;
+    &::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #fff;
+    }
+  }
+}
+</style>
